8512a3014686429d4b8f47be8400f62d100d1e42
ChangSpring <ChangSpring0728@gmail.com>
2018-05-08 16:06:36 +0800

    update

--- app/src/main/java/com/alfred/androidstudy/AlfredActivity.java
+++ app/src/main/java/com/alfred/androidstudy/AlfredActivity.java
@@ -2,17 +2,18 @@ package com.alfred.androidstudy;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
-import com.alfred.androidstudy.widget.HorizontalScrollTextView;
+import com.alfred.androidstudy.widget.HorizontalScrollImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class AlfredActivity extends AppCompatActivity {
 
-    @BindView(R.id.textview)
-    HorizontalScrollTextView textview;
+//    @BindView(R.id.textview)
+//    HorizontalScrollTextView textview;
+    @BindView(R.id.imageview)
+    HorizontalScrollImageView imageview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +22,22 @@ public class AlfredActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        textview.init();
-        textview.startScroll();
+//        textview.init();
+//        textview.startScroll();
+//
+//        textview.setOnScrollCompleteListener(new HorizontalScrollTextView.OnScrollCompleteListener() {
+//            @Override
+//            public void onScrollComplete() {
+//                Log.i("AlfredActivity","onScrollCompleted");
+//            }
+//        });
 
-        textview.setOnScrollCompleteListener(new HorizontalScrollTextView.OnScrollCompleteListener() {
+        imageview.startTranslate();
+
+        imageview.post(new Runnable() {
             @Override
-            public void onScrollComplete() {
-                Log.i("AlfredActivity","onScrollCompleted");
+            public void run() {
+
             }
         });
 
---
+++ app/src/main/java/com/alfred/androidstudy/widget/HorizontalScrollImageView.java
@@ -0,0 +1,96 @@
+package com.alfred.androidstudy.widget;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import com.alfred.androidstudy.R;
+
+/**
+ * Created by alfred on 2018/5/7.
+ */
+
+public class HorizontalScrollImageView extends AppCompatImageView {
+
+    private int mWidth, mHeight;
+    private Bitmap mBitmap;
+    private Paint mPaint;
+    private Rect mSrcRect, mDestRect;
+
+    public HorizontalScrollImageView(Context context) {
+        this(context, null);
+    }
+
+    public HorizontalScrollImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, -1);
+    }
+
+    public HorizontalScrollImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.test);
+
+        Log.i("Size", "widht = " + mBitmap.getWidth() + "  " + mBitmap.getHeight());
+
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+
+        mSrcRect = new Rect();
+        mDestRect = new Rect();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        //srcRect绘制Bitmap的哪一部分   destRect绘制的Bitmap拉伸到哪里
+        canvas.drawBitmap(mBitmap, mSrcRect, mDestRect, mPaint);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        mWidth = w;
+        mHeight = h;
+
+        //此值的位置是相对于view的左边距,而不是距离屏幕的距离!!!!!!!
+        mDestRect.top = 0;
+        mDestRect.bottom = mHeight;
+        //此值的位置是相对于view的左边距,而不是距离屏幕的距离!!!!!!!
+        mSrcRect.top = 0;
+        mSrcRect.bottom = mHeight;
+    }
+
+
+    public void startTranslate() {
+        ValueAnimator animator = ValueAnimator.ofFloat(0, 1);
+        animator.setDuration(1000);
+        animator.start();
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float currentLeft = (Float) animation.getAnimatedValue();
+                mSrcRect.left = mWidth - (int) (mWidth * currentLeft);
+                mSrcRect.right = mWidth;
+                //此值的位置是相对于view的左边距,而不是距离屏幕的距离!!!!!!!
+                mDestRect.left = 0;
+                mDestRect.right = (int) (mWidth * currentLeft);
+
+                postInvalidate();
+
+            }
+        });
+    }
+}
